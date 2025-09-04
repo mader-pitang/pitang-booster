118a8555bdaaaff9e307e3a19d3a73bf9d09afbc
Mader Gabriel <mader.gabriel@pitang.com>
2025-09-04 13:06:51 -0300

    Initial configuration of c1m1 project

---
+++ booster-c1m1/src/main/java/com/pitang/booster_c1m1/BoosterC1M1Application.java
@@ -0,0 +1,13 @@
+package com.pitang.booster_c1m1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosterC1M1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoosterC1M1Application.class, args);
+	}
+
+}
---
+++ booster-c1m1/src/test/java/com/pitang/booster_c1m1/BoosterC1M1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitang.booster_c1m1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoosterC1M1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
