e05633c94b51ee2b820e4fff555432b0d27ab521
Mader Gabriel <mader.gabriel@pitang.com>
2025-09-06 18:34:56 -0300

    Add User repository interface
    - Create UserRepository extending JpaRepository
    - Enable CRUD operations for User entity

---
+++ booster-c1m1/src/main/java/com/pitang/booster_c1m1/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.pitang.booster_c1m1.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pitang.booster_c1m1.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  boolean existsByEmail(String email);
+  Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
+}
